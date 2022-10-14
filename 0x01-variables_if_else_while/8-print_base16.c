3681e756737137951fca2d67ff61fcc3707024d2
trinkilipslups <trinkilipslups@gmail.com>
2022-10-14 15:26:57 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,16 @@
  */
 int main(void)
 {
-
 	int i;
 
 	for (i = 48; i < 58; i++)
-        {
+	{
 		putchar(i);
 	}
-        for (i = 97; i < 103; i++)
-        {
+	for (i = 97; i < 103; i++)
+	{
 		putchar(i);
 	}
-        putchar('\n');
+	putchar('\n');
 	return (0);
 }
