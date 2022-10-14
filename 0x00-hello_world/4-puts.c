0cfa07774aea5cdb76510453d42622065e908cca
trinkilipslups <trinkilipslups@gmail.com>
2022-10-14 07:32:01 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
+vim 4-puts.c
 #include <stdio.h>
 /**
-* main - Printing
-*
-* Return: Always 0 (Successful) 
-*/
+ * main - Printing
+ *
+ * Return: Always 0 (Successful) 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
