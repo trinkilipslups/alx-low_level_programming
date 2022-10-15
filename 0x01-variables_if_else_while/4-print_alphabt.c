c0230b11a4f870095b1deee507e6f9fefac55973
trinkilipslups <trinkilipslups@gmail.com>
2022-10-15 04:19:58 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if(i == 'e'|| i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
