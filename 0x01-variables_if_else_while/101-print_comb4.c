bb1e31199552d2e0fa2937e71f3382f5b6417c72
trinkilipslups <trinkilipslups@gmail.com>
2022-10-15 04:59:03 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,16 +11,16 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j ='0'; j <= '9'; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			for (m ='0'; m <= '9'; m++)
+			for (m = '0'; m <= '9'; m++)
 			{
-				if(i < j && j < m)
+				if (i < j && j < m)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(m);
-					if(i == '7' && j == '8' && m == '9')
+					if (i == '7' && j == '8' && m == '9')
 					{
 						break;
 					}
