7bf4a04b75ffb69b905dfc18d8795aa9f69a5a9e
trinkilipslups <trinkilipslups@gmail.com>
2022-10-31 13:55:20 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"                                                                                                   
+/**
+ *  * _memset - Fills the first n bytes of the memory area
+ *  @s: memory area to fill
+ *  @b: constant byte to fill
+ *  @n: bytes of memory area to fill
+ *  Return: the memory area filled
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n-- > 0)
+	{
+		*ptr = b;
+	}
+	return (s);
+}
