4ccd1f0b2e91d4a3d30e44b84e9fd27122d12969
trinkilipslups <trinkilipslups@gmail.com>
2022-11-09 10:44:00 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a program that prints all arguments
+ * @argc: the # of args
+ * @argv: the names of args
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+
+	while (argc--)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
